378a257df5bfdc515a28ad5788d9ca3173f75809
Ziad Abouelfarah <zain.fake02@gmail.com>
2020-04-26 20:34:06 +0000

    just basic idea

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const CORS = require("cors");
 const app = require("express")();
+const Discord = require("discord.js");
 
-app.use(CORS());
+const client = new Discord.Client();
+const channel = client.channels.get("name", "general");
 
-app.post("/", (req, res) => {
-  // HNA OUR CODE BAX NSEFTO MSG F DISCORD
+//Bot stuff
+client.on("ready", () => {
+  client.user.setPresence({
+    game: {
+      name: "Blood for NEXUS",
+    },
+  });
 });
 
-//
+app.use(CORS());
+app.post("/", (req, res) => {
+  channel.send(JSON.stringify(req.body));
+});
+
+client.login("NzA0MDYzMjI0NjEzNTY4NTEy.XqXtBQ.-e_qclqofygBi-tk50LM4VodGSo");
+
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Lol listing in ${process.env.PORT || 5000}`);
 });
